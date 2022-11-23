5e7eea165ded8816e570998bab95eb0490f1a493
Timothy Wardlow <wardlowtimothy@icloud.com>
2022-11-22 21:13:22 -0600

    testing POST route

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 const notes = require('./db/db.json');
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
