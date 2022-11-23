808f029bf1138328c4b70583a192a62e8649fd34
Timothy Wardlow <wardlowtimothy@icloud.com>
2022-11-22 21:33:18 -0600

    fixing logic for creating/displaying notes

--- server.js
+++ server.js
@@ -4,30 +4,14 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const notes = require('./db/db.json');
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-const notes = require('./db/db.json');
-
-function createNote(body, notesArr) {
-    const newNote = body;
-    notesArr.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({notes: notesArr }, null, 2)
-    );
-    return newNote;
-}
-
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-});
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const newNote = createNote(req.body, notes);
-    res.json(newNote);
 });
 
 app.get('/', (req, res) => {
@@ -36,6 +20,27 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '.public/index.html'));
+});
+
+
+function createNote(body, notesArr) {
+    const newNote = body;
+    notesArr.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArr, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const newNote = createNote(req.body, notes);
+    res.json(newNote);
 });
 
 app.listen(PORT, () => {
