3212c3bfd4c0ea27676d45daabe291c3ac608016
Timothy Wardlow <wardlowtimothy@icloud.com>
2022-11-22 21:18:48 -0600

    adding create new note functionality

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -7,13 +9,24 @@ app.use(express.json());
 
 const notes = require('./db/db.json');
 
+function createNote(body, notesArr) {
+    const newNote = body;
+    notesArr.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes: notesArr }, null, 2)
+    );
+    return newNote;
+}
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+    req.body.id = notes.length.toString();
+    const newNote = createNote(req.body, notes);
+    res.json(newNote);
 });
 
 app.listen(PORT, () => {
